e459b90a18ec36412dcfb263bcb2a0fb70d74899
Logan Manns <logan.manns@uky.edu>
2019-10-04 19:35:04 -0400

    Things still working after a few additions to PatientPage

--- MarkeyApp/patientPage.swift
+++ MarkeyApp/patientPage.swift
@@ -13,10 +13,18 @@ class patientPage : UITableViewController {
     var patientName : NSManagedObject?
     var patientLabelPlate :String?
     
-    func setuplabel(){
-    }
+    @IBOutlet var PatientInfoTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
         title=patientName!.value(forKeyPath: "name") as? String
+        PatientInfoTable.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        PatientInfoTable.dataSource=self
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = PatientInfoTable.dequeueReusableCell(withIdentifier: "Cell")!
+        return cell
     }
 }
